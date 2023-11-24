131bc17bb4b53b1ad0ed0a8cef0fe4eae64a07de
adityapatil18 <adityagangane2018@gmail.com>
2023-11-24 18:07:35 +0530

    chnages done

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,8 @@
-import 'package:d_app/view/screens/home_screen.dart';
 import 'package:d_app/view/screens/login_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-import 'utils/shared_functions.dart';
 
 SharedPreferences? _sharedPreferences;
 
